9d7d9787c31c172587a9b58c31caaf273781868a
Sunkara Bhavana <bhavanasunkara3@gmail.com>
2026-01-22 18:03:46 +0530

    Update ContactUsPage.jsx

--- src/Pages/ContactUsPage.jsx
+++ src/Pages/ContactUsPage.jsx
@@ -59,7 +59,7 @@ const ContactUsPage = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/api/contact", {
+            const response = await fetch("https://poizdedgebackend.onrender.com/api/contact", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -270,3 +270,4 @@ const ContactUsPage = () => {
 };
 
 export default ContactUsPage;
+
