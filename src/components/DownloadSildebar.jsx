60f09d7dc45e1cd8f6c20ba3a72d141bd6e1e3c3
Sunkara Bhavana <bhavanasunkara3@gmail.com>
2026-01-23 17:41:58 +0530

    Update DownloadSildebar.jsx

--- src/components/DownloadSildebar.jsx
+++ src/components/DownloadSildebar.jsx
@@ -46,7 +46,7 @@ const DownloadSildebar = () => {
               <span className="title-bold">Handbook</span> | Student Guidelines
             </p>
             {/* Go to Handbook page */}
-            <Link to=/handbook className="register-button">
+            <Link to="/handbook" className="register-button">
               Download Now <RegisterArrow />
             </Link>
           </div>
@@ -58,4 +58,5 @@ const DownloadSildebar = () => {
 };
 
 export default DownloadSildebar;
+
 
